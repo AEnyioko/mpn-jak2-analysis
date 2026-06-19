5c22eb28954010d154bf9e4baf5e57b824ca6ed4
ZeroChillz <aenyioko090@gmail.com>
2026-06-19 16:10:28 -0500

    feat(dataset): Add script for cdataset convertsion to h5ad

--- scripts/acquire.py
+++ scripts/acquire.py
@@ -1,8 +1,21 @@
+from fileinput import filename
+
 import numpy as np
-import adata as ad
+import anndata as ad
 import pandas as pd
 
 # acquire files
-def acqurie_dataset(import_path, output_path):
+def acquire_dataset(import_path, output_path):
   # csv's or tsv's
+  if import_path.endswith('.csv'):
+    df = pd.read_csv(import_path)
+  elif import_path.endswith('.tsv'):
+    df = pd.read_csv(import_path, sep='\t')
+  else:
+    raise ValueError("Unsupported file format")
   
+  # create AnnData object
+  adata = ad.AnnData(df)
+  adata.write_h5ad(filename, output_path)
+  # saving metadata as a string
+  obs = adata.obs.astype(str)
